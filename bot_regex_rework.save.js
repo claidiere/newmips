4329a435ecf012ab62b31692edd9ec901a129b31
Jerome Bonini <jerome.bonini@newmips.com>
2019-12-03 15:58:31 +0100

    - regex bot replace

---
+++ bot_regex_rework.save.js
@@ -0,0 +1,47 @@
+(?:create|add) component (?:contactform|contact form)(?: (?:called|with name) (\S+))?
+create component contactform with name (.*)
+create component contact form with name (.*)
+add component contactform with name (.*)
+add component contact form with name (.*)
+create component contactform called (.*)
+create component contact form called (.*)
+add component contactform called (.*)
+add component contact form called (.*)
+create component contactform
+create component contact form
+add component contactform
+add component contact form
+
+(?:créer|ajouter) (?:composant |un composant |le composant |un |le )?formulaire de contact(?: (?:appelé|nommé) (.+))?
+créer un composant formulaire de contact appelé (.*)
+ajouter un composant formulaire de contact appelé (.*)
+créer composant formulaire de contact appelé (.*)
+ajouter composant formulaire de contact appelé (.*)
+créer le composant formulaire de contact appelé (.*)
+ajouter le composant formulaire de contact appelé (.*)
+créer un formulaire de contact appelé (.*)
+ajouter un formulaire de contact appelé (.*)
+créer le formulaire de contact appelé (.*)
+ajouter le formulaire de contact appelé (.*)
+créer formulaire de contact appelé (.*)
+ajouter formulaire de contact appelé (.*)
+créer un composant formulaire de contact nommé (.*)
+ajouter un composant formulaire de contact nommé (.*)
+créer composant formulaire de contact nommé (.*)
+ajouter composant formulaire de contact nommé (.*)
+créer le composant formulaire de contact nommé (.*)
+ajouter le composant formulaire de contact nommé (.*)
+créer un formulaire de contact nommé (.*)
+ajouter un formulaire de contact nommé (.*)
+créer le formulaire de contact nommé (.*)
+ajouter le formulaire de contact nommé (.*)
+créer formulaire de contact nommé (.*)
+ajouter formulaire de contact nommé (.*)
+créer un composant formulaire de contact
+ajouter un composant formulaire de contact
+créer un formulaire de contact
+ajouter un formulaire de contact
+créer composant formulaire de contact
+ajouter composant formulaire de contact
+créer formulaire de contact
+ajouter formulaire de contact
